1f42aedb9664fa9114f3930013e22a62d00acc6d
Dang Duong <dangduong31205@gmail.com>
2024-05-24 21:32:07 +0700

    create solution.py

---
+++ solution.py
@@ -0,0 +1,8 @@
+class solution:
+    def dispatch():
+        pass
+        # your solution goes here
+        # return a list of dispatching actions (pair of job-matchine)
+
+        # a naive dispatching rule for an example
+
