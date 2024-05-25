9bb966fc38e294beb5d23c751a57640395a18d26
Dang Duong <dangduong31205@gmail.com>
2024-05-25 12:56:00 +0700

    update gentest.py

--- gentest.py
+++ gentest.py
@@ -12,28 +12,43 @@ import numpy as np
 import random
 import os
 
-MMACHINE = [5, 10, 10, 15, 20]
-NJOB = [5, 10, 30, 15, 20]
-NTEST = 1000
+NTEST = 5000
 
-t = np.zeros(shape=(100, 100, 100))
-
-for (M, N) in zip(MMACHINE, NJOB):
-    dirname = "./tests/{}_{}".format(M, N)
-
+if __name__ == "__main__":
+    N_init = np.random.choice([5, 10, 15], size=NTEST)
+    N_arrive = np.random.choice([10, 20, 30], size=NTEST)
+    M = np.random.randint(low=5, high=11, size=NTEST)
     E = np.random.choice([1, 5, 10], size=NTEST)
 
     for test in range(NTEST):
-        filename = "{}/{}.inp".format(dirname, test + 1)
+        N = N_init[test] + N_arrive[test]
+        machine_list = np.arange(start=1, stop=M[test] + 1, dtype=int)
 
-        tau = np.random.exponential(scale=E[test], size=N)
+        tau = np.append(np.zeros(shape=N_init[test]), np.random.exponential(scale=E[test], size=N_arrive[test]))
         for i in range(1, N):
             tau[i] += tau[i - 1]
         tau = np.floor(tau)
-        A = [ np.random.randint(t, t + 6) for t in tau ]
 
+        A = [ np.random.randint(t, t + 6) for t in tau ]
         W = np.random.randint(low=1, high=11, size=N)
+
+        # write test
+        f = open("./tests/{}.inp".format(test + 1), "w")
+
+        f.write('{} {}\n'.format(N, M[test]))
+        f.write(''.join((str(int(t)) + ' ') for t in tau) + '\n')
+        f.write(''.join((str(int(t)) + ' ') for t in A) + '\n\n')
+
         for i in range(N):
+            f.write(str(int(W[i])) + '\n')
             for j in range(W[i]):
-                random.sample(
+                Mj = np.random.randint(low=1, high=M[test] + 1)
+                M_list = random.sample(machine_list.tolist(), Mj)
+
+                f.write(str(int(Mj)) + ' ')
+                for machine in M_list:
+                    f.write(str(int(machine)) + ' ' + str(np.random.randint(1, 11)) + ' ')
+                f.write('\n')
+
+            f.write('\n\n')
 
